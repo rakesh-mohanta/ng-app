ecf53f35ae13141824a690f2636a222674e6c3fd
Rakesh <rakeshmohanta2k10@gmail.com>
2020-01-24 13:07:23 +0530

    better folder structure as per angular style guide

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 //import { NgAppComponent } from './ng-app/ng-app.component';
-import { HelloWorldComponent } from './hello-world/hello-world.component';
-import { UserItemComponent } from './user-item/user-item.component';
-import { UserListComponent } from './user-list/user-list.component';
+//import { HelloWorldComponent } from './hello-world/hello-world.component';
+//import { UserItemComponent } from './user-item/user-item.component';
+//import { UserListComponent } from './user-list/user-list.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
@@ -14,9 +14,9 @@ import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [
     AppComponent,
-    HelloWorldComponent,
-    UserItemComponent,
-    UserListComponent
+    //HelloWorldComponent,
+    //UserItemComponent,
+    //UserListComponent
   ],
   imports: [
     BrowserModule,
