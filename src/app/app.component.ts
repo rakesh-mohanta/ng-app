e0cf706a5d311a46bc3a33abfa60a67c0f78f22c
Rakesh <rakeshmohanta2k10@gmail.com>
2020-01-24 19:22:07 +0530

    header footer layout added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import {  ChangeDetectionStrategy, Component } from '@angular/core';
+import { NbIconConfig, NbComponentSize } from '@nebular/theme';
 
 @Component({
   selector: 'app-root',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  
 })
 export class AppComponent {
+  disabledIconConfig: NbIconConfig = { icon: 'settings-2-outline', pack: 'eva' };
   title = 'ng-app';
+  sizes: NbComponentSize[] = [ 'tiny', 'small', 'medium', 'large', 'giant' ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-//import { NgAppComponent } from './ng-app/ng-app.component';
-//import { HelloWorldComponent } from './hello-world/hello-world.component';
-//import { UserItemComponent } from './user-item/user-item.component';
-//import { UserListComponent } from './user-list/user-list.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
+import { NbThemeModule, NbLayoutModule, NbActionsModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    //HelloWorldComponent,
-    //UserItemComponent,
-    //UserListComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
@@ -24,6 +17,7 @@ import { AppRoutingModule } from './app-routing.module';
     NbThemeModule.forRoot({ name: 'default' }),
     NbLayoutModule,
     NbEvaIconsModule,
+    NbActionsModule,
     AppRoutingModule
   ],
   providers: [],
